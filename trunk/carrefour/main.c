1b6713c7766aea752d8341620bc99b056aa1f415
Alexis.Vallet@gmail.com <Alexis.Vallet@gmail.com@1e7e411d-715d-c436-aeb0-342d2c617efc>
2011-05-19 16:52:11 +0000

    Ajout d'un makefile, du planning, d'un readme ainsi que des conventions à suivre. Ajout d'un dossier rapport qui nous servira plus tard.

---
+++ trunk/carrefour/main.c
@@ -0,0 +1,14 @@
+#ifndef _MAIN_C_
+#define _MAIN_C_
+
+#include "main.h"
+
+/*
+  Point d'entrée du programme, affiche "Hello, world!", ignore les arguments.
+ */
+int main(int argc, char **argv) {
+  printf("Hello, world!\n");
+  return 0;
+}
+
+#endif
---
+++ trunk/carrefour/main.h
@@ -0,0 +1,12 @@
+/*
+  Module principale du programme.
+ */
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv);
+
+#endif
