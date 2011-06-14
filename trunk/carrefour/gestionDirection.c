cea0d6431f5e5fcf3d44201430d53a139086973c
Alexis.Vallet@gmail.com <Alexis.Vallet@gmail.com@1e7e411d-715d-c436-aeb0-342d2c617efc>
2011-06-14 09:28:18 +0000

    Ajout d'un semaphore, pour les acces a la memoire partagee. Quelques ajouts dans la gestion des directions.

--- trunk/carrefour/gestionDirection.c
+++ trunk/carrefour/gestionDirection.c
@@ -3,6 +3,20 @@
 
 #include "gestionDirection.h"
 
+/* La probabilité qu'une voiture arrive dans une direction donnée. */
+#define PROBA_ARRIVEE_VOITURE 0.75
+/* La probabilité qu'un bus arrive dans une directions donnée. */
+#define PROBA_ARRIVEE_BUS     0.25
+/* La probabilité qu'un véhicule puisse sortir de sa voie de sortie. */
+#define PROBA_SORTIE_VEHICULE 0.5
+
+#define TAILLE_VOIE_ENTREE    20
+#define TAILLE_VOIE_SORTIE    20
+
+/* Gère la direction dir, avec nombreVoiesVoiture voies de voitures et nombreVoiesBus
+   voies de bus. Les directions de la voie de voiture numero i est donnée dans
+   tableDirections[i], et les direction de la voie de bus numero j est donnée dans
+   tableDirections[nombreVoiesVoiture + j], où i et j commencent à zero. */
 void gestionDirection(const direction dir, const int *fileRequetesBus, 
 		      const int memoireEtatFeu,	const int nombreVoiesVoiture,
 		      const direction *tableDirections,
