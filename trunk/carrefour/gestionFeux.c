181a27fb01c5f428c077b71f3e47b65e0477a80b
henri.maugendre@gmail.com <henri.maugendre@gmail.com@1e7e411d-715d-c436-aeb0-342d2c617efc>
2011-06-20 14:16:54 +0000

    Mise a jour de GestionFeu.h et .c inclusion des temps

--- trunk/carrefour/gestionFeux.c
+++ trunk/carrefour/gestionFeux.c
@@ -1,40 +1,55 @@
 #ifndef _GESTIONFEUX_C_
 #define _GESTIONFEUX_C_
 
+
 #include "gestionFeux.h"
 
-void gestionFeux(const int fileRequetesBus, const int memoireEtatFeu,
-		 const int semaphoreEtatFeux, const int memoireVoiesSortie,
-		 const int semaphoreVoiesSortie) {
-  /*
-     requeteBus requete;
-     requeteBus.type = 42;
-     feu *etat = NULL;
-     etat = shmat(memoireEtatFeu, NULL, 0);
-
-     while (true) {
-     msgrcv(fileRequeteBus, &requete, 5, &requete.type, IPC_NOWAIT);
-     if (&requete.direction == 1) {
-     if (*etat == VERT_EST_OUEST) {
-     strcpy(etat, "VERT_NORD_SUD");
-     }
-     } else if (&requete.direction == 2) {
-     if (*etat == VERT_EST_OUEST) {
-     strcpy(etat, "VERT_NORD_SUD");
-     }
-     } else if (&requete.direction == 4) {
-     if (*etat == VERT_NORD_SUD) {
-     strcpy(etat, "VERT_EST_OUEST");
-     }
-     } else if (&requete.direction == 8) {
-     if (*etat == VERT_NORD_SUD) {
-     strcpy(etat, "VERT_EST_OUEST");
-     }
-     }
-
-     }
-
-     return; */
+void gestionFeux(const int fileRequetesBus, const int memoireEtatFeu, const int semaphoreEtatFeux, const int memoireVoiesSortie, const int semaphoreVoiesSortie) {
+		 
+  requeteBus requete;
+  requete.type=42;
+  feu *etat = NULL;
+  etat = shmat(memoireEtatFeu, NULL, 0);
+  clock_t tempsDebut;
+  
+  while (1) {
+     tempsDebut=clock();
+    
+    while(msgrcv(fileRequetesBus, &requete, 5, requete.type, IPC_NOWAIT)==-1) {
+      if(((clock()-tempsDebut)/CLOCKS_PER_SEC)%7==0) {
+        if(*etat==VERT_EST_OUEST) {
+          *etat=VERT_NORD_SUD;
+        }
+        else if(*etat==VERT_NORD_SUD) {
+          *etat=VERT_EST_OUEST;
+        }
+      }
+    }
+    
+    if(requete.contenantLeBus==1) {
+      if(*etat==VERT_EST_OUEST) {
+       *etat=VERT_NORD_SUD;
+      }
+    }
+    else if(requete.contenantLeBus==2) {
+      if(*etat==VERT_EST_OUEST) {
+        *etat=VERT_NORD_SUD;
+      }
+    }
+    else if(requete.contenantLeBus==4) {
+      if(*etat==VERT_NORD_SUD) {
+        *etat=VERT_EST_OUEST;
+      }
+    }
+    else if(requete.contenantLeBus==8) {
+      if(*etat==VERT_NORD_SUD) {
+        *etat=VERT_EST_OUEST;
+      }
+    }
+    
+  }
+		 
+  return;
 }
 
 
--- trunk/carrefour/gestionFeux.h
+++ trunk/carrefour/gestionFeux.h
@@ -1,6 +1,17 @@
 #ifndef _GESTIONFEUX_H_
 #define _GESTIONFEUX_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#include "types.h"
+#include <time.h>
+
 /* Procédure de gestion des feux du carrefour. filesRequetesBus[direction] 
    est l'identifiant d'une file de messages contenant les demandes de 
    passage au vert des bus de la direction direction. memoireEtatFeu est
